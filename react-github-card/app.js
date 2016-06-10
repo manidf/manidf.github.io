1ba727f2af4bcb8b953b1fd3c99129093f05c4a4
manidf <mannuel.ferreira@gmail.com>
2016-06-10 07:20:05 +0200

    github card

--- react-github-card/app.js
+++ react-github-card/app.js
@@ -1,11 +1,24 @@
-var Main = React.createClass({
+
+var Card = React.createClass({
 	render: function() {
 		return (
 			<div>
-
+				<img src="https://avatars.githubusercontent.com/u/210504?v=3" width="200" />
+				<h3>Name here</h3>
+				<hr />
 			</div>
 		)
 	}
 });
 
-React.render(<Main/>, document.getElementById("root"));
+var Main = React.createClass({
+	render: function() {
+		return (
+			<div>
+		Hello
+			</div>
+		)
+	}
+});
+
+React.render(<Main />, document.getElementById("root"));
