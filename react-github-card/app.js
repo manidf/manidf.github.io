e5bda4fbcde3d42d7e202f6f33c81c527f37408e
manidf <mannuel.ferreira@gmail.com>
2016-06-11 00:26:14 +0200

    moar items, er um actually people

--- react-github-card/app.js
+++ react-github-card/app.js
@@ -14,7 +14,7 @@ var Card = React.createClass({
 			<div>
 
 				<div className="mdl-grid">
-	  			<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet">
+	  			<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
 						<div className="demo-card-wide mdl-card mdl-shadow--2dp">
 						  <div className="mdl-card__title">
 								<img className="mdl-card_image" src={this.state.avatar_url} />
@@ -44,7 +44,7 @@ var Card = React.createClass({
 
 var Main = React.createClass({
 	getInitialState: function() {
-		return { logins: ['manidf', 'MaggotMouth', 'nicja', 'matthewgoslette']}
+		return { logins: ['MaggotMouth', 'nicja', 'manidf', 'r00ster', 'etiennemarais', 'BradWhittington', 'garykrige']}
 	},
 	render: function() {
 		var cards = this.state.logins.map(function(login) {
