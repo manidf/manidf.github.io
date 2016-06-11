a5924caee87c417b255ac71812b990d688ce50c2
manidf <mannuel.ferreira@gmail.com>
2016-06-11 08:22:38 +0200

    styling stuffs not making it work , making it pretty

--- react-github-card/app.jsx
+++ react-github-card/app.jsx
@@ -11,40 +11,36 @@ var Card = React.createClass({
 	},
 	render: function() {
 		return (
-			<div>
 
-				<div className="mdl-grid">
-	  			<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
-						<div className="demo-card-wide mdl-card mdl-shadow--2dp">
-						  <div className="mdl-card__title">
-								<img className="mdl-card_image" src={this.state.avatar_url} />
-						    <h2 className="mdl-card__title-text">{this.state.name}</h2>
-						  </div>
-						  <div className="mdl-card__supporting-text">
-						    Followers: {this.state.followers}
-						  </div>
-						  <div className="mdl-card__actions mdl-card--border">
-						    <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-						      FOLLOW
-						    </a>
-						  </div>
-						  <div className="mdl-card__menu">
-						    <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-						      <i className="material-icons">share</i>
-						    </button>
-						  </div>
-						</div>
-					</div>
+			<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
+				<div className="demo-card-wide mdl-card mdl-shadow--2dp">
+				  <div className="mdl-card__title">
+						<img className="mdl-card_image" src={this.state.avatar_url} />
+				    <h2 className="mdl-card__title-text">{this.state.name}</h2>
+				  </div>
+				  <div className="mdl-card__supporting-text">
+				    Followers: {this.state.followers}
+				  </div>
+				  <div className="mdl-card__actions mdl-card--border">
+				    <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+				      FOLLOW
+				    </a>
+				  </div>
+				  <div className="mdl-card__menu">
+				    <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+				      <i className="material-icons">share</i>
+				    </button>
+				  </div>
 				</div>
-
 			</div>
+
 		)
 	}
 });
 
 var Main = React.createClass({
 	getInitialState: function() {
-		return { logins: ['zoidbergwilli', 'MaggotMouth', 'nicja', 'manidf', 'r00ster', 'etiennemarais', 'BradWhittington', 'garykrige', 'johnmarkmassey']}
+		return { logins: ['zoidbergwill', 'MaggotMouth', 'nicja', 'manidf', 'r00ster', 'etiennemarais', 'BradWhittington', 'garykrige', 'johnmarkmassey']}
 	},
 	render: function() {
 		var cards = this.state.logins.map(function(login) {
